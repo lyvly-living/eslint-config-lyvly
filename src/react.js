ffea0c5b2d44fbfe96d1cc16df4b31660c796389
Ray Parkar <ray@lyvly.uk>
2020-03-06 11:48:15 +0000

    Allow default exports in react

--- src/react.js
+++ src/react.js
@@ -1,13 +1,21 @@
 module.exports = {
   parser: "babel-eslint",
+
   extends: [
     "eslint-config-airbnb",
     "eslint-config-prettier",
     "./rules/lyvly",
     "./rules/prettier"
   ].map(require.resolve),
+
   plugins: ["jest"],
+
   env: {
     "jest/globals": true
+  },
+
+  rules: {
+    // In React world, we allow default exports.
+    "import/no-default-export": "off"
   }
 };
