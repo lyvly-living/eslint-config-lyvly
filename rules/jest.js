d620229869ae664ce37fde2b6d5a7ac05248db4e
mattdean1 <matt.dean.001@gmail.com>
2020-04-17 09:55:29 +0100

    Change jest rules to have padding around all blocks

--- rules/jest.js
+++ rules/jest.js
@@ -2,8 +2,8 @@ module.exports = {
   plugins: ["jest", "jest-formatting"],
   extends: ["plugin:jest/recommended"],
   rules: {
-    // We want a linebreak between it() blocks
-    "jest-formatting/padding-around-test-blocks": 2
+    // Make sure there is a linebreak between all jest blocks
+    "jest-formatting/padding-around-all": 2
   },
   env: {
     "jest/globals": true
